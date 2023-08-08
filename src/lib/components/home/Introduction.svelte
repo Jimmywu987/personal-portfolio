1507d29d6a1e949cb09805af663a3df2c9670105
Jimmy Wu <jimmy@wemakeapp.net>
2023-08-09 00:27:49 +0100

    feat: add html tag ui

--- src/lib/components/home/Introduction.svelte
+++ src/lib/components/home/Introduction.svelte
@@ -5,19 +5,38 @@
 
 <HtmlTag tag="head">
   <span class="h-[80vh] flex flex-col justify-center items-center space-y-2">
-    <h1 class="text-left flex flex-col justify-center text-xl">
-      <HtmlTag tag="title" classAttribute="text-center"
+    <h1
+      class="w-full lg:w-5/6 text-left flex flex-col justify-center text-xl lg:text-5xl"
+    >
+      <HtmlTag tag="title" classAttribute="ml-10"
         >Hi! I'm Jimmy, a Software Developer.</HtmlTag
       >
     </h1>
     <p class=" w-full lg:w-5/6">
       <HtmlClosingTag
         tag="meta"
-        attribute="description"
+        attribute="email"
+        attributeValue={`" 
+    jimmywu987@gmail.com
+    "`}
+      />
+    </p>
+    <p class=" w-full lg:w-5/6">
+      <HtmlClosingTag
+        tag="meta"
+        attribute="phone"
+        attributeValue={`" 
+    (+44) 07407654182
+    "`}
+      />
+    </p>
+    <p class=" w-full lg:w-5/6">
+      <HtmlClosingTag
+        tag="meta"
+        attribute="about me"
         attributeValue={`" 
         I am passionate in computer programming. Having gained experiences as a Full Stack Developer from Freehunter and WeMakeApp and with the motivation of continuous self-learning, I am ready to take on more challenges to dive deeper into the tech industry.
         "`}
-        classAttribute="lg:ml-10 "
       />
     </p>
   </span>
